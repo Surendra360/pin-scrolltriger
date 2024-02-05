00bfc99e17436241fe63f2b6fe64a54ddf60398e
Surendra Kumar <88983549+Surendra360@users.noreply.github.com>
2024-02-05 23:03:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+gsap.to(".page2 h1",{
+    transform:"translateX(-150%)",
+    scrollTrigger:
+    {   
+        trigger:".page2",
+        scroller:"body",
+        scrub:2,
+        markers:true,
+        start:"top 0",
+        end:"top -100%",
+        pin:true,
+    }
+})
